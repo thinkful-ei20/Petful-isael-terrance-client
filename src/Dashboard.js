108b3dfed37b829c9cc0df6702af5b65ab5881b4
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-06-29 11:44:32 -0600

    created dashboard component

---
+++ src/Dashboard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function Dashboard(props) {
+  return <section>
+    <article>
+      <img src='https://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-cat-photos-funny-cover.jpg' alt='Heavy orange bengal cat'/>
+      <h2>Lardy</h2>
+      <dl>
+        <dt>sex</dt>
+        <dd>male</dd>
+        <dt>age</dt>
+        <dd>4</dd>
+        <dt>breed</dt>
+        <dd>Bengal</dd>
+        <dt>story</dt>
+        <dd>Ate too much</dd>
+      </dl>
+      <button>Adopt</button>
+    </article>
+    <article>
+    <img src="https://i.imgur.com/yXDNi58.jpg"/>
+      <h2>Lardy</h2>
+      <dl>
+        <dt>sex</dt>
+        <dd>male</dd>
+        <dt>age</dt>
+        <dd>4</dd>
+        <dt>breed</dt>
+        <dd>Bengal</dd>
+        <dt>story</dt>
+        <dd>Ate too much</dd>
+      </dl>
+      <button>Adopt</button>
+    </article>
+  </section>;
+}
